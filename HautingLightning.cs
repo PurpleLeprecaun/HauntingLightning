ea20f270ff7a0c8a7a1776be3dd841982e5d99ce
Ethan Nixon <ethan.nixon62@gmail.com>
2020-09-26 17:57:57 -0700

    Add project files.

---
+++ HautingLightning.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace PurpleLeprecaun
+{
+    public class HauntingLightning
+    {
+        public void Awake()
+        {
+            On.EntityStates.FrozenState.OnEnter += (orig, self) =>
+            {
+                Chat.AddMessage("[Lightning Mcqueen]: I fucked your mom shitlips");
+
+                orig(self);
+            };
+        }
+    }
+}
